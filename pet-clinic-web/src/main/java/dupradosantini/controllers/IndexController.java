f83d596b0310181cbd02293d2ecfe89647f2593c
dupradosantini <dupradosantini@opus-software.com.br>
2022-02-04 14:15:59 -0300

    Vets Index page and Controller  Closes #7

--- pet-clinic-web/src/main/java/dupradosantini/controllers/IndexController.java
+++ pet-clinic-web/src/main/java/dupradosantini/controllers/IndexController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class indexController {
+public class IndexController {
     @RequestMapping({"","index","index.html","/"})
     String index(){
         return "index"; //it will look for a template called index, which we created.
---
+++ pet-clinic-web/src/main/java/dupradosantini/controllers/VetController.java
@@ -0,0 +1,14 @@
+package dupradosantini.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class VetController {
+
+    @RequestMapping({"/vets","/vets/index", "/vets/index.html"})
+    public String listVets(){
+        return "vets/index"; //Telling the string with the path to the html file (thymeleaf template file)
+    }
+
+}
