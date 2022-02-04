a1d726b560a281b2e2aaa6595268317df5406056
dupradosantini <dupradosantini@opus-software.com.br>
2022-02-04 10:20:51 -0300

    Creating Services interfaces #5

---
+++ pet-clinic-data/src/main/java/dupradosantini/services/VetService.java
@@ -0,0 +1,14 @@
+package dupradosantini.services;
+
+import dupradosantini.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findById(Long id);
+
+    Vet save(Vet vet);
+
+    Set<Vet> findAll();
+}
