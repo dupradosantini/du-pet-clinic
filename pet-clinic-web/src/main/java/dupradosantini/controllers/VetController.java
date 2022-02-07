90e7460333765695daef20be71f6331de586bdbb
dupradosantini <dupradosantini@opus-software.com.br>
2022-02-07 11:07:34 -0300

    Adding Vet List and updated controller. Closes #15

--- pet-clinic-web/src/main/java/dupradosantini/controllers/VetController.java
+++ pet-clinic-web/src/main/java/dupradosantini/controllers/VetController.java
@@ -1,13 +1,25 @@
 package dupradosantini.controllers;
 
+import dupradosantini.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class VetController {
 
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
+
     @RequestMapping({"/vets","/vets/index", "/vets/index.html"})
-    public String listVets(){
+    public String listVets(Model model){
+
+        model.addAttribute("vets",vetService.findAll());
+
         return "vets/index"; //Telling the string with the path to the html file (thymeleaf template file)
     }
 
