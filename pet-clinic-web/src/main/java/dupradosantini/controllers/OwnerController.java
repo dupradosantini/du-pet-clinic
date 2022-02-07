ed9761d5a1d5e2c19263ab94052102d8fa10f29d
dupradosantini <dupradosantini@opus-software.com.br>
2022-02-07 10:56:48 -0300

    Adding Owner List and updated controller. Closes #16

--- pet-clinic-web/src/main/java/dupradosantini/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/dupradosantini/controllers/OwnerController.java
@@ -1,13 +1,26 @@
 package dupradosantini.controllers;
 
+import dupradosantini.services.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/owners")//Prefixing to everything
 @Controller
 public class OwnerController {
+
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
+
     @RequestMapping({"/","/index", "/index.html", ""})//Everything is prefixed with owners
-    public String listOwners(){
+    public String listOwners(Model model){
+
+        model.addAttribute("owners",ownerService.findAll());
+
         return "/owners/index";
     }
 }
